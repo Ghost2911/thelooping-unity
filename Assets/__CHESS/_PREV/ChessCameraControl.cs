8063cbea8725c7a7c6e2eb13f670041f25a21612
Ghost2911 <45088431+Ghost2911@users.noreply.github.com>
2022-10-12 18:26:27 +0300

    Chesser, few changes

---
+++ Assets/__CHESS/_PREV/ChessCameraControl.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class ChessCameraControl : MonoBehaviour
+{
+	public float speed = 5.0f;
+	public Vector3 closePosition = new Vector3(0,0,0);
+	public Vector3 closeRotationVector = new Vector3(0,0,0);
+	private Quaternion closeRotation;
+
+	private Vector3 farPosition = new Vector3(0,0,0); 
+	private Quaternion farRotation;
+	private bool isFar = true;
+
+	private void Start()
+	{
+		closeRotation = Quaternion.Euler(closeRotationVector);
+		farPosition = transform.position;
+		farRotation = transform.rotation;
+	}
+	
+	public void SwapCameraPosition()
+	{
+		StopAllCoroutines();
+		StartCoroutine(EnumCameraPosition());
+	}
+
+	public IEnumerator EnumCameraPosition ()
+	{
+		Quaternion targetRotation = (isFar) ? closeRotation : farRotation;
+		Vector3 targetPosition = (isFar) ? closePosition : farPosition;
+		isFar = !isFar;
+
+		while (Vector3.Distance(transform.position,targetPosition)>0.1f)
+		{
+			transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, Time.deltaTime * speed);
+			transform.position = Vector3.Lerp(transform.position, targetPosition, Time.deltaTime * speed);
+
+			yield return null;
+		}
+	}
+}
